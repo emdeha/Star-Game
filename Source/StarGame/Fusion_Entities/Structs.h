c7320ec69f778a34e1ba6712ca3f4f2613972f8e
cpi-cecko <cpi.cecko@gmail.com>
2013-07-22 13:01:14 +0300

    started to add enemies and solar system

---
+++ Source/StarGame/Fusion_Entities/Structs.h
@@ -0,0 +1,43 @@
+//Copyright 2012, 2013 Tsvetan Tsvetanov
+//This file is part of the Star Game.
+//
+//The Star Game is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//The Star Game is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with the Star Game.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#include <vector>
+#include "../glsdk/glm/glm.hpp"
+
+
+struct PatrolRoute
+{
+	std::vector<glm::vec3> patrolPoints;
+	glm::vec3 lastVelocity;
+	glm::vec3 nextPatrolPoint;
+	unsigned int currentPatrolPointIndex;
+	unsigned int nextPatrolPointIndex;
+	unsigned int lastPatrolPointIndex;
+
+	PatrolRoute() :
+		patrolPoints(0), lastVelocity(glm::vec3()), nextPatrolPoint(glm::vec3()), 
+		currentPatrolPointIndex(0), nextPatrolPointIndex(0), lastPatrolPointIndex(0) {}
+};
+
+struct DamageOverTime
+{
+	unsigned int damage;
+	unsigned int time_seconds;
+
+	DamageOverTime() :
+		damage(0), time_seconds(0) {}
+};

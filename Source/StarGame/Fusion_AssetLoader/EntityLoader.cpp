7d12a20ebb25a452f000e465be659819d25fb67b
cpi-cecko <gogo_rus@abv.bg>
2014-02-22 12:21:59 +0200

    created functions which return the script corresponding to its stringed name

--- Source/StarGame/Fusion_AssetLoader/EntityLoader.cpp
+++ Source/StarGame/Fusion_AssetLoader/EntityLoader.cpp
@@ -3,9 +3,94 @@
 
 #include "../Fusion_Entities/Skill.h"
 #include "../Fusion_Entities/Components.h"
+#include "../Fusion_Entities/Scripts.h"
 
 using namespace FusionEngine;
 
+
+OnUpdateFunc GetOnUpdateMethodByID(const std::string &id)
+{
+	if (id == "AOE_OnUpdate")
+	{
+		return AOE_OnUpdate;
+	}
+	else if (id == "Burn_OnUpdate")
+	{
+		return Burn_OnUpdate;
+	}
+	else if (id == "SunNova_OnUpdate")
+	{
+		return SunNova_OnUpdate;
+	}
+	else if (id == "Chaining_OnUpdate")
+	{
+		return Chaining_OnUpdate;
+	}
+	else if (id == "Shield_OnUpdate")
+	{
+		return Shield_OnUpdate;
+	}
+	else if (id == "Frost_OnUpdate")
+	{
+		return Frost_OnUpdate;
+	}
+
+	return nullptr;
+}
+OnEventFunc GetOnFusionCompletedMethodByID(const std::string &id)
+{
+	if (id == "SatelliteCreation_OnFusionCompleted")
+	{
+		return SatelliteCreation_OnFusionCompleted;
+	}
+	else if (id == "Ultimate_OnFusionCompleted")
+	{
+		return Ultimate_OnFusionCompleted;
+	}
+	else if (id == "AOE_OnFusionCompleted")
+	{
+		return AOE_OnFusionCompleted;
+	}
+	else if (id == "Burn_OnFusionCompleted")
+	{
+		return Burn_OnFusionCompleted;
+	}
+	else if (id == "SunNova_OnFusionCompleted")
+	{
+		return SunNova_OnFusionCompleted;
+	}
+	else if (id == "PassiveAOE_OnFusionCompleted")
+	{
+		return PassiveAOE_OnFusionCompleted;
+	}
+	else if (id == "Chaining_OnFusionCompleted")
+	{
+		return Chaining_OnFusionCompleted;
+	}
+	else if (id == "Shield_OnFusionCompleted")
+	{
+		return Shield_OnFusionCompleted;
+	}
+	else if (id == "Frost_OnFusionCompleted")
+	{
+		return Frost_OnFusionCompleted;
+	}
+
+	return nullptr;
+}
+OnEventFunc GetOnClickMethodByID(const std::string &id)
+{
+	if (id == "AOE_OnClick")
+	{
+		return AOE_OnClick;
+	}
+	else if (id == "Burn_OnClick")
+	{
+		return Burn_OnClick;
+	}
+
+	return nullptr;
+}
 
 EntityAssetObject EntityLoader::Load(const std::string &type, const std::string &name)
 {
